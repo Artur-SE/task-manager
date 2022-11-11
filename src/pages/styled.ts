b20e51a0c07aa89133bbdee956377a6420ff8792
Kacper <kacperfps@googlemail.com>
2022-11-11 22:11:08 +0100

    handleSubmit changed, styles applied

--- src/pages/styled.ts
+++ src/pages/styled.ts
@@ -1,10 +1,38 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const StyledMainPage = styled.main`
-    width: 100vw;
+export const StyledMain = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  h1 {
+    padding: 1rem;
+  }
+`;
+export const StyledTasks = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    padding: 1rem;
+  }
+`;
+
+export const StyledInputForm = styled.form`
+  width: 20vw;
+  padding: 1rem;
+  background-color: var(--secondaryBackgroundColor);
+  display: flex;
+  flex-direction: column;
+  margin-top: 0.5rem;
+  border-radius: 0.5rem;
+
+  .input__field {
     display: flex;
-    justify-content: center;
-    h1 {
-        padding: 1rem;
-    }
-`
+    justify-content: space-between;
+    margin-bottom: 0.5rem;
+  }
+`;
